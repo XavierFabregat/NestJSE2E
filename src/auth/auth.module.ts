ba6cfcb1239ddf150490c9cc2a5232b2b8307d2a
XavierFabregat <xavi.fabregat.pous@gmail.com>
2023-11-26 22:42:05 +0100

    fix(auth): implement validate function for jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategy';
+import { PrismaService } from '../prisma/Prisma.service';
 
 @Module({
   imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, PrismaService],
 })
 export class AuthModule {}
